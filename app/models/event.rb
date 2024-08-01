d5027d668d690ae15a920d65fcca4c839ba15127
Uzawa Ren <96781771+uzawa-ren@users.noreply.github.com>
2024-08-01 19:49:18 +0000

    Add validations to event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,7 @@
 class Event < ApplicationRecord
+  validates :name, presence: true
+  validates :location, presence: true
+  validates :date, presence: true
+
   belongs_to :creator, class_name: "User"
 end
