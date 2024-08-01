3ecda37172515fb43789f51eb0ed497b75e72844
Uzawa Ren <96781771+uzawa-ren@users.noreply.github.com>
2024-08-01 12:41:03 +0000

    Add creating event form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,12 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = params[:user].events.build(params[:event])
+  end
 end
