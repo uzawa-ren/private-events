9895fd5fe25ea0a41cabfadf3670338454bc8f7b
Uzawa Ren <96781771+uzawa-ren@users.noreply.github.com>
2024-08-01 14:09:44 +0000

    Add form for creating an event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,12 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+  
   def index
     @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
   end
 
   def new
@@ -8,6 +14,20 @@ class EventsController < ApplicationController
   end
 
   def create
-    @event = params[:user].events.build(params[:event])
+    @event = current_user.events.build(event_params)
+
+    if @event.save
+      flash[:success] = "Your event has been created."
+      redirect_to @event
+    else
+      flash.now[:error] = "Your event cannot be created due to errors."
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :location, :date, :creator, :description)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_31_211243) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_01_141431) do
   create_table "events", force: :cascade do |t|
     t.date "date"
     t.string "location"
@@ -30,6 +30,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_31_211243) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
